7b266c9fd4f0eb8164d89b66e8beb56172d65064
Johan Groth <johan.groth@crunch.co.uk>
2016-06-07 14:08:16 +0100

    Still no luck getting instrumentation test to work.

--- app/src/androidTest/java/uk/org/linuxgrotto/tocrunch/api/impl/ClientTemplateInstrumentationTest.java
+++ app/src/androidTest/java/uk/org/linuxgrotto/tocrunch/api/impl/ClientTemplateInstrumentationTest.java
@@ -1,22 +1,34 @@
 package uk.org.linuxgrotto.tocrunch.api.impl;
 
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Map;
+
+import uk.org.linuxgrotto.tocrunch.R;
 import uk.org.linuxgrotto.tocrunch.activity.ToCrunchActivity;
 
 import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.action.ViewActions.click;
-import static org.hamcrest.Matchers.allOf;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 
 /**
  * Created by jgroth on 07/06/16.
@@ -29,9 +41,66 @@ public class ClientTemplateInstrumentationTest {
     public ActivityTestRule<ToCrunchActivity> activityTestRule = new ActivityTestRule<>(ToCrunchActivity.class);
 
     @Test
-    public void test() {
-        onData(allOf(is(instanceOf(String[].class)), hasEntry(equalTo("STR"), is("Accounts")))).perform(click());
+    public void testDataItemInAdapter() {
+        // onView(withId(R.id.tocrunch_activity_options_list)).perform(click()).check(matches(isDisplayed()));
+        onData(instanceOf(ToCrunchActivity.class)).inAdapterView(withId(R.id.tocrunch_activity_options_list)).atPosition(1)
+            .check(matches(hasDescendant(withText("Accounts"))));
 
+    }
+
+    private static Matcher<View> withAdaptedData(final Matcher<Object> dataMatcher) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with class name: ");
+                dataMatcher.describeTo(description);
+            }
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof AdapterView)) {
+                    return false;
+                }
+                @SuppressWarnings("rawtypes")
+                Adapter adapter = ((AdapterView) view).getAdapter();
+                for (int i = 0; i < adapter.getCount(); i++) {
+                    if (dataMatcher.matches(adapter.getItem(i))) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+    }
+
+    /**
+     * Creates a matcher against the text stored in R.id.item_content. This text is roughly
+     * "item: $row_number".
+     */
+    public static Matcher<Object> withItemContent(String expectedText) {
+        // use preconditions to fail fast when a test is creating an invalid matcher.
+        checkNotNull(expectedText);
+        return withItemContent(equalTo(expectedText));
+    }
+
+    /**
+     * Creates a matcher against the text stored in R.id.item_content. This text is roughly
+     * "item: $row_number".
+     */
+    @SuppressWarnings("rawtypes")
+    public static Matcher<Object> withItemContent(final Matcher<String> itemTextMatcher) {
+        // use preconditions to fail fast when a test is creating an invalid matcher.
+        checkNotNull(itemTextMatcher);
+        return new BoundedMatcher<Object, Map>(Map.class) {
+            @Override
+            public boolean matchesSafely(Map map) {
+                return hasEntry(equalTo("STR"), itemTextMatcher).matches(map);
+            }
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with item content: ");
+                itemTextMatcher.describeTo(description);
+            }
+        };
     }
 
 }
